6e1eddcb6894db96900b4efff9bd4bce9d8996da
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2025-01-25 21:30:27 +0700

    add exit function for user input

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-
 def main():
-    user_input = input("What graph do you want to plot? : ")
-    print(f"Your answer : {user_input}")
-
+    while True:
+        user_input = input("What graph do you want to plot? (Type 'exit' to quit): ")
+        if "exit" in user_input.lower():
+            print("Thank you – that’s it for today, bye!")
+            break
+        print(f"Your answer: {user_input}")
 
 if __name__ == "__main__":
     main()
